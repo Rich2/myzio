b8c91cbdbc582947ca9d9bbc9b7ab31c765867bf
Rich2 <rr1@tutanota.com>
2023-09-24 21:27:13 +0100

    Now using zio.http

--- MyZioApp.scala
+++ MyZioApp.scala
@@ -1,7 +1,20 @@
 import zio._
 import zio.Console._
+import zio.http._
+import zio.http.html._
 
-object MyZioApp extends ZIOAppDefault {
+object HelloWorld extends ZIOAppDefault {
+
+  val app: App[Any] = 
+    Http.collect[Request] {
+      case Method.GET -> Root => Response.html("This is the home page")
+    }
+
+  override val run =
+    Server.serve(app).provide(Server.default)
+}
+
+/*object MyZioApp extends ZIOAppDefault {
 
   def run = myAppLogic
 
@@ -11,4 +24,4 @@ object MyZioApp extends ZIOAppDefault {
       name <- readLine
       _    <- printLine(s"Hello, ${name}, welcome to ZIO!")
     } yield ()
-}
+}*/
--- myzio.sbt
+++ myzio.sbt
@@ -1,1 +1,2 @@
 libraryDependencies += "dev.zio" %% "zio" % "2.0.17"
+libraryDependencies += "dev.zio" %% "zio-http" % "3.0.0-RC2"

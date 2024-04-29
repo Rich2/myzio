e5180dad57555e75d7f2a8e4ddb0c1c2afaeb40f
Rich2 <rr1@tutanota.com>
2024-04-29 23:13:00 +0100

    Updated Zio and ZioHttp.

--- MyZioApp.scala
+++ MyZioApp.scala
@@ -1,27 +1,13 @@
-import zio._
-import zio.Console._
-import zio.http._
-import zio.http.html._
+import zio._, Console._, http._
 
-object HelloWorld extends ZIOAppDefault {
+object HelloWorld extends ZIOAppDefault
+{ val app: HttpApp[Any] =
+    Routes(
+      Method.GET / "index.html" -> handler(Response.html("This is the home page")),
+      Method.GET / "text" -> handler(Response.html("This is the text page"))
 
-  val app: App[Any] = 
-    Http.collect[Request] {
-      case Method.GET -> Root => Response.html("This is the home page")
-    }
+  ).toHttpApp
 
   override val run =
     Server.serve(app).provide(Server.default)
 }
-
-/*object MyZioApp extends ZIOAppDefault {
-
-  def run = myAppLogic
-
-  val myAppLogic =
-    for {
-      _    <- printLine("Hello! What is your name?")
-      name <- readLine
-      _    <- printLine(s"Hello, ${name}, welcome to ZIO!")
-    } yield ()
-}*/
--- myzio.sbt
+++ myzio.sbt
@@ -1,2 +1,2 @@
-libraryDependencies += "dev.zio" %% "zio" % "2.0.17"
-libraryDependencies += "dev.zio" %% "zio-http" % "3.0.0-RC2"
+libraryDependencies += "dev.zio" %% "zio" % "2.1.0-RC3"
+libraryDependencies += "dev.zio" %% "zio-http" % "3.0.0-RC6"
